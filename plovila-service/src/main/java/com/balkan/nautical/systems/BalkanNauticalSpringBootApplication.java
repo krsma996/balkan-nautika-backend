0ba1f33c420da64395e4bde9dd5be0d82e9c49d8
NikolaKrsmanovic1996 <nikolakrsmanovic223@gmail.com>
2026-01-09 17:17:46 +0100

    Small fix
    Added property dependecy for H2 mssql and mysql and configured app.properties

--- plovila-service/src/main/java/com/balkan/nautical/systems/BalkanNauticalSpringBootApplication.java
+++ plovila-service/src/main/java/com/balkan/nautical/systems/BalkanNauticalSpringBootApplication.java
@@ -3,14 +3,15 @@ package com.balkan.nautical.systems;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+import org.springframework.context.annotation.PropertySource;
+import org.springframework.context.annotation.PropertySources;
 
 
 @SpringBootApplication
+@PropertySource("file:${pho.home.directory}/application.properties")
 public class BalkanNauticalSpringBootApplication extends SpringBootServletInitializer {
 
 	public static void main(String[] args) {
 		SpringApplication.run(BalkanNauticalSpringBootApplication.class, args);
-
 	}
-
 }
